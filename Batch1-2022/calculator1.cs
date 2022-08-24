e81c00c44461623eadf7d202b5e328e1f73fb0d8
SoundaryaKalal <104731104+SoundaryaKalal@users.noreply.github.com>
2022-08-24 05:56:17 -0700

    Add files via upload

---
+++ Batch1-2022/calculator1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_2022
+{
+    public class Calculator1
+    {
+        public static void Main()
+        {
+
+        }
+        public static int add(int a, int b)
+        {
+            return a + b;
+        }
+}
+    }
